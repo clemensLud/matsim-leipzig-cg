cf8d3e37c7a841368e3bfcdccee54909b795bc6f
hzoerner <h.zoerner@campus.tu-berlin.de>
2022-09-06 15:57:38 +0200

    added analysis for income distribution

--- src/main/R/matsim-by-age-analysis.R
+++ src/main/R/matsim-by-age-analysis.R
@@ -280,3 +280,90 @@ ggplot(compare.2.2.diff, aes(age_bin, diff, fill = neg)) +
 savePlotAsJpg(name = "Age_Group_Diff_Plot")
 
 rm(srv.2.1, srv.2.2, trips.2.1, trips.2.2, compare.2.1, compare.2.2, compare.2.1.diff, compare.2.2.diff)
+
+#### compare income distribution between matsim and SrV ####
+
+SRV.H = "C:/Users/ACER/Desktop/Uni/VSP/NaMAV/data/SrV_2018/SrV2018_Einzeldaten_Leipzig_LE_SciUse_H2018.csv"
+
+srv.h = read.csv2(SRV.H)
+
+inc.breaks = c(-Inf, 0,1,2,3,4,5)
+inc.labels = c("No Information", "< 500 €", "500 < 1500 €", "1500 < 3600 €", "3600 < 5600 €", "> 5600 €")
+inc.breaks.2 = c(-Inf, 0, 500, 1500, 3600, 5600, Inf)
+
+econ.breaks = c(-Inf, 0,1,2,3,4,5)
+econ.labels = c("No Information", "Sehr niedrig", "Niedrig", "Mittel", "Hoch", "Sehr hoch")
+
+srv.h.1 = srv.h %>%
+  transmute(hh_nr = HHNR, hh_weight = GEWICHT_HH, E_EINK_5, E_OEK_STATUS) %>%
+  mutate(income_bin = cut(E_EINK_5, breaks = inc.breaks, labels = inc.labels),
+         src = "SrV") %>%
+  select(-starts_with("E_"))
+
+trips.3 = trips.1 %>%
+  select(person, income) %>%
+  group_by(person) %>%
+  summarise(n_trips = n(),
+            income = first(income)) %>%
+  mutate(income = as.numeric(income),
+         income_bin = cut(income, breaks = inc.breaks.2, labels = inc.labels),
+         src = "MATSim")
+
+trips.3.1 = trips.3 %>%
+  select(income_bin, src) %>%
+  group_by(income_bin) %>%
+  summarise(count = n(),
+            src = first(src)) %>%
+  mutate(share = count / sum(count))
+
+compare.3 = srv.h.1 %>%
+  select(income_bin, hh_weight, src) %>%
+  group_by(income_bin) %>%
+  summarise(count = sum(hh_weight),
+            src = first(src)) %>%
+  mutate(share = count / sum(count)) %>%
+  filter(income_bin != "No Information") %>%
+  group_by(src) %>%
+  mutate(total = sum(share),
+         new_share = share / total) %>%
+  select(-c(share, total)) %>%
+  rename("share" = "new_share") %>%
+  bind_rows(trips.3.1)
+
+ggplot(compare.3, aes(x = income_bin, y = share, fill = src)) +
+  
+  geom_col() +
+  
+  scale_y_continuous(breaks = seq(0, 0.7, 0.1)) +
+  
+  labs(x = "Income Group", y = "Share") +
+  
+  facet_wrap(. ~ src) +
+  
+  theme_bw() +
+  
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90))
+
+savePlotAsJpg(name = "Income_Distribution")
+
+compare.3.diff = compare.3 %>%
+  select(-count) %>%
+  pivot_wider(names_from = src, values_from = share, values_fill = 0.0) %>%
+  mutate(diff = MATSim - SrV,
+         neg = ifelse(diff < 0, T, F))
+
+ggplot(compare.3.diff, aes(income_bin, diff, fill = neg)) +
+  
+  geom_col() +
+  
+  coord_flip() +
+  
+  labs(y = "MATSim - SrV", x = "Income Group", title = "Difference of MATSim income distribution compared to SrV 2018") +
+  
+  theme_bw() +
+  
+  theme(legend.position = "none",
+        plot.title = element_text(size = 15))
+
+savePlotAsJpg(name = "Income_Distribution_Diff")
